9b4ef4333e5f9ae5c6783e1b35530c26cff856c6
corynissen <corynissen@gmail.com>
2013-07-26 13:25:21 -0500

    added slider for date

--- foodborne-tweets/server.R
+++ foodborne-tweets/server.R
@@ -6,7 +6,7 @@ source("update_data.R")  # df is all the data
 load("data.Rdata")
 
 shinyServer(function(input, output) {
-  
+
   data <- reactive({    
     if(input$refresh > 0){
       isolate({
@@ -25,24 +25,45 @@ shinyServer(function(input, output) {
     
     df
   })
-    
+
+  # had to subset the data after the slider was created
+  subset.data <- reactive({
+    df <- data()
+    max.date <- max(df$created_at3)
+    df <- subset(df, created_at3 >= max.date-days(input$day.slider.reactive))
+    df
+  })
+
+  # create the slider here because I need input from the df dataframe
+  output$day.slider <- renderUI({
+    df <- data()
+    min.date <- min(df$created_at3)
+    max.date <- max(df$created_at3)
+    max.value <- ceiling(as.numeric((max.date - min.date)))
+    #browser()
+    return(sliderInput("day.slider.reactive", "Date range (back from present)",
+                       min=1, max=max.value, value=7))
+  })  
+
   output$caption <- renderText({
     "Foodborne Chicago Tweets by Time"
   })
 
   output$plot <- renderPlot({
-    df <- data()
+    df <- subset.data()
     tmp <- as.data.frame(table(df$created_at2, df$category))
     tmp$Var1 <- as.character(tmp$Var1)
     tmp$Var2 <- as.character(tmp$Var2)    
     
     p <- ggplot(tmp) + geom_point(aes(x=Var1, y=Freq, colour=Var2)) +
-         geom_line(aes(x=Var1, y=Freq, colour=Var2, group=Var2))
+         geom_line(aes(x=Var1, y=Freq, colour=Var2, group=Var2)) +
+         xlab("Date") + ylab("Count of Tweets") +
+         scale_colour_discrete(name="Tweet Label")
     print(p)
   })
 
   output$tweet.table <- renderTable({
-      df <- data()
+      df <- subset.data()
       tab <- subset(df, select=c("text", "category", "created_at"))      
   },include.rownames=FALSE)
       
